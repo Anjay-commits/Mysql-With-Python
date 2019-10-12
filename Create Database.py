cd81c38c46a075ea3e7d7225302cd29d0d81bf24
anjay <kumaranjay8@gmail.com>
2019-10-12 16:54:58 +0530

    Databse creation Using Python

--- Create Database.py
+++ Create Database.py
@@ -9,7 +9,4 @@ mydb = pymysql.connect(
 
 mycursor = mydb.cursor()
 
-#mycursor.execute("CREATE DATABASE IF NOT EXISTS myd")
-mycursor.execute("show ")
-for i in mycursor:
-    print(i)
+mycursor.execute("CREATE DATABASE IF NOT EXISTS myd")
